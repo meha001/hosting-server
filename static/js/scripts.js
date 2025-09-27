a3e8eed0731d67bf78ac1d306534e49ec9278de0
Mehrubon <nabievm811@gmail.com>
2025-09-27 13:13:17 +0300

    1

--- static/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById('loginBtn').addEventListener('click', function() {
-//         window.location.href = this.getAttribute('href');
-//     });
-// });
