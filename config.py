579cc358138dc68cd38a9ad5d48b41679bb6d0f6
Mehrubon <nabievm811@gmail.com>
2025-09-27 13:10:37 +0300

    1

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'user_files')
+    WTF_CSRF_ENABLED = True
