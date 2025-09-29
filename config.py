a2eb0be46e0391a32cf091acca3933197c3becc7
Mehrubon <nabievm811@gmail.com>
2025-09-29 17:16:03 +0300

    1

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'rkmlkmkklmfmrmgrg'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret") 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'user_files')
+    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", os.path.join(os.getcwd(), 'user_files'))
     WTF_CSRF_ENABLED = True
